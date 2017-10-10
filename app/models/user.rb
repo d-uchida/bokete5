dbf17dff7904467532407b96b5f6c38896c3f82f
内田　大輔 <z15.uchida.daisuke@gmail.com>
2017-10-10 19:56:59 +0900

    Delete user model & table and Create devise user model and table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  has_many :bokes
-  has_many :odais
-  has_many :comments
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'bokes#index'
   resources :bokes
   # get 'bokes' => 'bokes#index'
