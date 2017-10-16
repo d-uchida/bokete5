b000500879521f0d502d26fce74c564959654b13
内田　大輔 <z15.uchida.daisuke@gmail.com>
2017-10-16 22:36:37 +0900

    Change second_odai into ode

--- app/models/odai.rb
+++ app/models/odai.rb
@@ -1,6 +1,6 @@
-# class Odai < ApplicationRecord
-#   belongs_to :photo
-#   has_many :bokes
-#   has_many :comments, through: :bokes
-#   has_one :user, through: :photo
-# end
+class Odai < ApplicationRecord
+  belongs_to :photo
+  has_many :bokes
+  has_many :comments, through: :bokes
+  has_one :user, through: :photo
+end
