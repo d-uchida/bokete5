fe1c48f83a8fccbae44eb818ba585627dc2f1ffd
内田　大輔 <z15.uchida.daisuke@gmail.com>
2017-10-20 19:38:27 +0900

    Set routing for done page

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,14 @@ Rails.application.routes.draw do
   resources :bokes, only: [:index, :new, :create, :preview] do
     collection do
       post 'preview'
+      get 'done'
     end
   end
   resources :odais, only: [:index, :new, :create, :preview] do
     resources :bokes
     collection do
       post 'preview'
+      get 'done'
     end
   end
   resources :photos, only: [:index, :new, :create, :upload]
